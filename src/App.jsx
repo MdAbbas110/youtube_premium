d0aa01358a62bcfe326669c39d7238e687346a65
abbas <meerabbas110@gmail.com>
2025-03-10 15:46:12 +0530

    project setup with components structure and redux store

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Head from "./components/Head";
+import Body from "./components/Body";
+
+function App() {
+  return (
+    <div>
+      <Head />
+      <Body />
+    </div>
+  );
+  /**
+   * head
+   * body
+   * aside bar
+   *   menu items
+   * mainContainer
+   *   button list
+   *   video container
+   *   video card
+   */
+}
+
+export default App;
---
+++ src/components/Head.jsx
@@ -0,0 +1,30 @@
+import { Menu, SearchSlash, User, Youtube } from "lucide-react";
+import React from "react";
+
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1 items-center ">
+        <Menu className="size-7" />
+        <span className="flex items-center mx-2">
+          <Youtube className="size-8" color="red" />
+          <p className="text-lg ml-1 font-semibold">Premium</p>
+        </span>
+      </div>
+      <div className="col-span-10 flex items-center justify-center ">
+        <input
+          className="w-1/2 border border-gray-400 rounded-l-full p-2 "
+          type="text"
+        />
+        <div className="rounded-r-full border-r border-t border-b px-5 py-2 bg-gray-100 border-gray-400">
+          <SearchSlash />
+        </div>
+      </div>
+      <div className="col-span-1 flex justify-end ">
+        <User className="size-8" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
